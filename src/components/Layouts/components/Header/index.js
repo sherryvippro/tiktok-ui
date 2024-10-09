fdf69d0d2ade052e62b06a549c1bb9924dc110ef
sherryvippro <sherryvippro@gmail.com>
2024-10-10 02:13:32 +0700

    build UI Header #4

--- src/components/Layouts/components/Header/index.js
+++ src/components/Layouts/components/Header/index.js
@@ -1,16 +1,42 @@
 import { useEffect, useState } from 'react'
 import classNames from 'classnames/bind'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faCircleXmark, faMagnifyingGlass, faSpinner } from '@fortawesome/free-solid-svg-icons'
+import {
+    faCircleQuestion,
+    faCircleXmark,
+    faEarthAsia,
+    faEllipsisVertical,
+    faKeyboard,
+    faMagnifyingGlass,
+    faSpinner,
+} from '@fortawesome/free-solid-svg-icons'
 import Tippy from '@tippyjs/react/headless'
 
 import styles from './Header.module.scss'
 import images from '~/assets/images/logo.svg'
+
 import { Wrapper as PopperWrapper } from '~/components/Popper'
 import AccountItem from '~/components/AccountItem'
 import Button from '~/components/Button'
+import Menu from '~/components/Popper/Menu'
 
 const cx = classNames.bind(styles)
+
+const MENU_ITEMS = [
+    {
+        icon: <FontAwesomeIcon icon={faEarthAsia} />,
+        title: 'English',
+    },
+    {
+        icon: <FontAwesomeIcon icon={faCircleQuestion} />,
+        title: 'Feedback and help',
+        to: '/feedback',
+    },
+    {
+        icon: <FontAwesomeIcon icon={faKeyboard} />,
+        title: 'Keyboard shortcuts',
+    },
+]
 
 function Header() {
     const [searchResult, setSearchResult] = useState([])
@@ -31,7 +57,7 @@ function Header() {
                         interactive
                         visible={searchResult.length > 0}
                         render={(attrs) => (
-                            <div className={cx('search-result')} tabIndex="1">
+                            <div className={cx('search-result')} tabIndex="1" {...attrs}>
                                 <PopperWrapper>
                                     <h4 className={cx('search-title')}>Accounts</h4>
                                     <AccountItem />
@@ -58,6 +84,12 @@ function Header() {
                 <div className={cx('action')}>
                     <Button text>Upload</Button>
                     <Button primary>Log in</Button>
+
+                    <Menu items={MENU_ITEMS}>
+                        <button className={cx('more-btn')}>
+                            <FontAwesomeIcon icon={faEllipsisVertical} />
+                        </button>
+                    </Menu>
                 </div>
             </div>
         </header>
