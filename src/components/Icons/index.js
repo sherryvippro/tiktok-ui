f8dec7928b0f2a9a9cc3060232ce98415b8648af
sherryvippro <sherryvippro@gmail.com>
2024-10-18 15:51:38 +0700

    build logic Header #1

--- src/components/Icons/index.js
+++ src/components/Icons/index.js
@@ -1,4 +1,4 @@
-export const MessageIcon = ({ width = '3.2rem', height = '3.2rem', className }) => (
+export const MessageIcon = ({ width = '2.6rem', height = '2.6rem', className }) => (
     <svg
         className={className}
         width={width}
@@ -34,7 +34,7 @@ export const NotificationIcon = ({ width = '3.2rem', height = '3.2rem', classNam
     </svg>
 )
 
-export const UploadIcon = ({ width = '3.2rem', height = '3.2rem', className }) => (
+export const UploadIcon = ({ width = '1.8rem', height = '1.8rem', className }) => (
     <svg
         className={className}
         width={width}
@@ -58,7 +58,7 @@ export const SearchIcon = ({ width = '2.4rem', height = '2.4rem', className }) =
         width={width}
         height={height}
         viewBox="0 0 48 48"
-        fill="rgba(22, 24, 35, 0.34)"
+        fill="currentColor"
         xmlns="http://www.w3.org/2000/svg"
     >
         <path
--- src/components/Layouts/components/Header/index.js
+++ src/components/Layouts/components/Header/index.js
@@ -1,20 +1,17 @@
-import { Fragment, useEffect, useState } from 'react'
+import { Fragment } from 'react'
 import classNames from 'classnames/bind'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import {
     faCircleHalfStroke,
     faCircleQuestion,
-    faCircleXmark,
     faCoins,
     faEarthAsia,
     faEllipsisVertical,
     faGear,
     faKeyboard,
     faSignOut,
-    faSpinner,
     faUser,
 } from '@fortawesome/free-solid-svg-icons'
-import HeadlessTippy from '@tippyjs/react/headless'
 import Tippy from '@tippyjs/react'
 import 'tippy.js/dist/tippy.css'
 
@@ -22,11 +19,10 @@ import styles from './Header.module.scss'
 import images from '~/assets/images/logo.svg'
 import rosie from '~/assets/images/rosie.jpg'
 import Image from '~/components/Images'
-import { Wrapper as PopperWrapper } from '~/components/Popper'
-import AccountItem from '~/components/AccountItem'
 import Button from '~/components/Button'
 import Menu from '~/components/Popper/Menu'
-import { MessageIcon, NotificationIcon, SearchIcon, UploadIcon } from '~/components/Icons'
+import { MessageIcon, NotificationIcon, UploadIcon } from '~/components/Icons'
+import Search from '../Search'
 
 const cx = classNames.bind(styles)
 const currentUser = true
@@ -84,13 +80,6 @@ const MENU_ITEMS = [
 ]
 
 function Header() {
-    const [searchResult, setSearchResult] = useState([])
-    useEffect(() => {
-        setTimeout(() => {
-            setSearchResult([])
-        }, 0)
-    }, [])
-
     const handleMenuClick = (menuItem) => {
         switch (menuItem.type) {
             case 'language':
@@ -131,35 +120,7 @@ function Header() {
                 <div className={cx('logo')}>
                     <img src={images} alt="TikTok" height="42" width="118" />
                 </div>
-                <div>
-                    <HeadlessTippy
-                        interactive
-                        visible={searchResult.length > 0}
-                        render={(attrs) => (
-                            <div className={cx('search-result')} tabIndex="1" {...attrs}>
-                                <PopperWrapper>
-                                    <h4 className={cx('search-title')}>Accounts</h4>
-                                    <AccountItem />
-                                    <AccountItem />
-                                    <AccountItem />
-                                    <AccountItem />
-                                </PopperWrapper>
-                            </div>
-                        )}
-                    >
-                        <div className={cx('search')}>
-                            <input placeholder="Search accounts and videos" spellCheck={false} />
-                            <button className={cx('clear')}>
-                                <FontAwesomeIcon icon={faCircleXmark} />
-                            </button>
-                            <FontAwesomeIcon className={cx('spinner')} icon={faSpinner} />
-
-                            <button className={cx('search-btn')}>
-                                <SearchIcon />
-                            </button>
-                        </div>
-                    </HeadlessTippy>
-                </div>
+                <Search />
                 <div className={cx('actions')}>
                     {currentUser ? (
                         <>
@@ -175,6 +136,7 @@ function Header() {
                             <Tippy content="Notification" placement="bottom">
                                 <button className={cx('action-btn')}>
                                     <NotificationIcon content="Notification" />
+                                    <span className={cx('badge')}>12</span>
                                 </button>
                             </Tippy>
                         </>
